55595e33af680b0a9cf65252605b4c5519a77337
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-11-27 19:42:30 +0800

    get post initial implementation

--- simple-web-service/src/api/api.js
+++ simple-web-service/src/api/api.js
@@ -7,3 +7,24 @@ const {
   UpdateItemCommand,
 } = require('@aws-sdk/client-dynamodb');
 const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
+
+const getPost = async event => {
+  const response = { statusCode: 200 };
+
+  try {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({ postId: event.pathParameters.postId }),
+    };
+    const { Item } = await db.send(new GetItemCommand(params));
+
+    console.log({ Item });
+    response.body = JSON.stringify({
+      message: 'Successfully retrieved post.',
+      data: Item ? unmarshall(Item) : {},
+      rawData: Item,
+    });
+  } catch (e) {}
+
+  return response;
+};
