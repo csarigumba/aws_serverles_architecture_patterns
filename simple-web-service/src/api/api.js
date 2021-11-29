6f50b6e7b7e890db9f1a98d26992d0463ab0339c
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-11-29 12:24:40 +0800

    refactor

--- simple-web-service/src/api/api.js
+++ simple-web-service/src/api/api.js
@@ -2,35 +2,6 @@ const db = require('./db');
 const { GetItemCommand, DeleteItemCommand, ScanCommand, UpdateItemCommand } = require('@aws-sdk/client-dynamodb');
 const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
 
-const getPost = async event => {
-  const response = { statusCode: 200 };
-
-  try {
-    const params = {
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      Key: marshall({ postId: event.pathParameters.postId }),
-    };
-    const { Item } = await db.send(new GetItemCommand(params));
-
-    console.log({ Item });
-    response.body = JSON.stringify({
-      message: 'Successfully retrieved post.',
-      data: Item ? unmarshall(Item) : {},
-      rawData: Item,
-    });
-  } catch (e) {
-    console.error(e);
-    response.statusCode = 500;
-    response.body = JSON.stringify({
-      message: 'Failed to get post.',
-      errorMsg: e.message,
-      errorStack: e.stack,
-    });
-  }
-
-  return response;
-};
-
 const updatePost = async event => {
   const response = { statusCode: 200 };
 
