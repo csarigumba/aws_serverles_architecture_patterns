df6255aaf0209f4c0f58c20ac7a5a7404a59b103
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-11-29 11:52:45 +0800

    refactor

--- simple-web-service/src/api/api.js
+++ simple-web-service/src/api/api.js
@@ -1,11 +1,5 @@
 const db = require('./db');
-const {
-  GetItemCommand,
-  PutItemCommand,
-  DeleteItemCommand,
-  ScanCommand,
-  UpdateItemCommand,
-} = require('@aws-sdk/client-dynamodb');
+const { GetItemCommand, DeleteItemCommand, ScanCommand, UpdateItemCommand } = require('@aws-sdk/client-dynamodb');
 const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
 
 const getPost = async event => {
@@ -29,34 +23,6 @@ const getPost = async event => {
     response.statusCode = 500;
     response.body = JSON.stringify({
       message: 'Failed to get post.',
-      errorMsg: e.message,
-      errorStack: e.stack,
-    });
-  }
-
-  return response;
-};
-
-const createPost = async event => {
-  const response = { statusCode: 200 };
-
-  try {
-    const body = JSON.parse(event.body);
-    const params = {
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      Item: marshall(body || {}),
-    };
-    const createResult = await db.send(new PutItemCommand(params));
-
-    response.body = JSON.stringify({
-      message: 'Successfully created post.',
-      createResult,
-    });
-  } catch (e) {
-    console.error(e);
-    response.statusCode = 500;
-    response.body = JSON.stringify({
-      message: 'Failed to create post.',
       errorMsg: e.message,
       errorStack: e.stack,
     });
@@ -167,7 +133,6 @@ const getAllPosts = async () => {
 
 module.exports = {
   getPost,
-  createPost,
   updatePost,
   deletePost,
   getAllPosts,
