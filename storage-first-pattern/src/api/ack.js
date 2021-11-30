e9aac88f6c56d48fc6dc566d72e803df4e1b44b1
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-11-30 20:07:08 +0800

    added dynamodb

--- storage-first-pattern/src/api/ack.js
+++ storage-first-pattern/src/api/ack.js
@@ -1,5 +1,6 @@
 const handler = async event => {
   // Add busines logic here
+  // TODO: Add logic to interact with DynamoDb here. But not urgent, the previous patterns has enough examples already.
   console.log(`Received event: ${JSON.stringify(event)}`);
   const response = {
     statusCode: 200,
