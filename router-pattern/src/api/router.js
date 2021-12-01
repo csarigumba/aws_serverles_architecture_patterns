c49057681ab3d9e26941fd6866a20e59a80f1d28
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-12-01 22:30:36 +0800

    working impl for router

--- router-pattern/src/api/router.js
+++ router-pattern/src/api/router.js
@@ -1,7 +1,15 @@
+const { LambdaClient, InvokeCommand } = require('@aws-sdk/client-lambda');
+const client = new LambdaClient();
+
 const handler = async event => {
   console.log(`Received event: ${JSON.stringify(event)}`);
 
-  processMode(event.pathParameters.mode);
+  try {
+    await processMode(event.pathParameters.mode);
+  } catch (error) {
+    console.log(error);
+  }
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
@@ -12,14 +20,29 @@ const handler = async event => {
   return response;
 };
 
-const processMode = mode => {
+const processMode = async mode => {
   console.log(`Mode: ${mode}`);
+
+  const input = {
+    InvocationType: 'Event',
+    Payload: JSON.stringify({
+      mode,
+    }),
+  };
+
   if (mode === 'mode-a') {
+    input.FunctionName = 'service-a';
   } else if (mode === 'mode-b') {
+    input.FunctionName = 'service-b';
   } else if (mode === 'mode-c') {
+    input.FunctionName = 'service-c';
   } else {
     // handle error handling here
+    return;
   }
+
+  const command = new InvokeCommand(input);
+  await client.send(command);
 };
 
 module.exports = {
