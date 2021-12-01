5c90afbfabc1c778bbc953c8b9347b81c5284aa1
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-12-01 21:24:03 +0800

    router impl

--- router-pattern/src/api/router.js
+++ router-pattern/src/api/router.js
@@ -1,14 +1,25 @@
 const handler = async event => {
-  // Add busines logic here
-  // TODO: Add logic to interact with DynamoDb here. But not urgent, the previous patterns has enough examples already.
   console.log(`Received event: ${JSON.stringify(event)}`);
+
+  processMode(event.pathParameters.mode);
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Successfully run logic.',
+      message: 'Done!',
+      event: event,
     }),
   };
   return response;
+};
+
+const processMode = mode => {
+  console.log(`Mode: ${mode}`);
+  if (mode === 'mode-a') {
+  } else if (mode === 'mode-b') {
+  } else if (mode === 'mode-c') {
+  } else {
+    // handle error handling here
+  }
 };
 
 module.exports = {
