c1acac4bd6ae580951357f9da77ab6438fb638db
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-12-01 21:17:23 +0800

    updated services impl

---
+++ router-pattern/src/services/service-b.js
@@ -0,0 +1,15 @@
+const handler = async event => {
+  // Process Service-B logic here
+  console.log(`Received event: ${JSON.stringify(event)}`);
+  console.log(`Mode: ${event.mode}`);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Successfully run logic.',
+    }),
+  };
+};
+
+module.exports = {
+  handler,
+};
